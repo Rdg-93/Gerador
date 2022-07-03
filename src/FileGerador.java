50e3c58e3bebf372c5db85e02a7cf8558d61e3d1
rdg <rdg@localhost.localdomain>
2022-07-03 00:53:26 -0300

    Initial commit

---
+++ src/FileGerador.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class FileGerador {
+    public static void main(String args[]) throws IOException {
+
+        String path = "/home/rdg/Documentos/Gerado";
+        ManipuladorArquivo.leitor(path);
+    }
+}
---
+++ src/ManipuladorArquivo.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ManipuladorArquivo {
+
+    public static void leitor(String path) throws IOException {
+        BufferedReader buffRead = new BufferedReader(new FileReader(path));
+        String linha = "";
+        while (true) {
+            if (linha != null) {
+                System.out.println(linha);
+
+            } else
+                break;
+            linha = buffRead.readLine();
+        }
+        buffRead.close();
+    }
+
+    public static void escritor(String path) throws IOException {
+        BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+        int tel = 911111111;
+        for (int i = 0;i<99999999;i++) {
+            buffWrite.append("11" + tel + i + "\n");
+        }
+        buffWrite.close();
+    }
+
+}
